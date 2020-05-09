8b1ec2f29ccf50201bf1b619ea90cbb2ca642f38
KeaporaTheOwl <56273951+KeaporaTheOwl@users.noreply.github.com>
2020-05-09 18:59:00 -0500

    Finished Sound

---
+++ Assets/Scripts/PlayReveal.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayReveal : MonoBehaviour
+{
+    private AudioSource revealAudio;
+    private bool alreadyRevealed;
+
+    private void Awake()
+    {
+        revealAudio = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player" && !alreadyRevealed)
+        {
+            revealAudio.Play();
+            alreadyRevealed = true;
+        }
+    }
+
+}
